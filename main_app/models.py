9606d6a9134b42bd03fef835611eb2caec3bc6ad
saadkhan29 <sunny_khan_29@yahoo.com>
2025-06-22 11:17:59 +0300

    Checkpoint MtoM

--- main_app/models.py
+++ main_app/models.py
@@ -10,12 +10,23 @@ MEALS = (
   ('D', 'Dinner'),
 )
 
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+  
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
 class Cat(models.Model):
   name = models.CharField(max_length=100) # Text Box
   breed = models.CharField(max_length=100) # Text Box
   description = models.TextField(max_length=250) # Text Area
   age = models.IntegerField() # Int Field
   image = models.ImageField(upload_to='main_app/static/uploads', default="")
+  toys = models.ManyToManyField(Toy)
 
   def get_absolute_url(self):
     return reverse('detail', kwargs={'cat_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Cat
+from .models import Cat, Toy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from .forms import FeedingForm
 
 # Create your views here.
@@ -50,9 +51,15 @@ def cats_index(request):
 def cats_detail(request, cat_id):
   cat = Cat.objects.get(id=cat_id)
   feeding_form = FeedingForm()
+
+  # Exclude those toys ids which exists in cat_toys join table with the current cat id.
+  # Remaining toys ids will be returned to toys_cat_doesnt_have.
+  toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
+
   return render(request, 'cats/detail.html', 
                 {'cat': cat, 
-                'feeding_form': feeding_form
+                'feeding_form': feeding_form,
+                'toys': toys_cat_doesnt_have
                 })
 
 def add_feeding(request, cat_id):
@@ -62,3 +69,31 @@ def add_feeding(request, cat_id):
     new_feeding.cat_id = cat_id
     new_feeding.save()
   return redirect('detail', cat_id=cat_id)
+
+
+class ToyList(ListView):
+  model = Toy 
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy 
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
+
+
+def assoc_toy(request, cat_id, toy_id):
+  Cat.objects.get(id=cat_id).toys.add(toy_id)
+  return redirect('detail', cat_id = cat_id)
+
+def unassoc_toy(request, cat_id, toy_id):
+  Cat.objects.get(id=cat_id).toys.remove(toy_id)
+  return redirect('detail', cat_id = cat_id)
